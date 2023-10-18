697fc8b0c705553321bd76130a999ef4f995801a
Angélica Rafaela <angelmaluh@gmail.com>
2023-10-18 09:53:21 -0300

    second commint

---
+++ src/test/java/br/com/angin/winxinevolution/WinxinevolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.angin.winxinevolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinxinevolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
